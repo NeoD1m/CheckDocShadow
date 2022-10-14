c682f7249db1478d78af3274319aae5a0aded575
admin <roman.wowsq@gmail.com>
2022-10-14 21:51:40 +0300

    Фиттедь боксик 2

--- lib/pages/greeting_page/greeting_page.dart
+++ lib/pages/greeting_page/greeting_page.dart
@@ -3,6 +3,7 @@ import 'package:checkdoc/pages/greeting_page/greeting_widget.dart';
 import 'package:checkdoc/pages/greeting_page/step_row.dart';
 import 'package:checkdoc/pages/greeting_page/upload_button.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class GreetingPage extends StatelessWidget {
   const GreetingPage({super.key});
@@ -12,19 +13,24 @@ class GreetingPage extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Center(
-        child: FittedBox(
-          fit: BoxFit.fitHeight,
-          child: Container(
-            //margin: EdgeInsets.only(left: (100.w - 1440.px) / 2),
-            width: 1440,
-            height: 1024,
-            child: Stack(
-              children: const [
-                BackgroundWidget(),
-                GreetingWidget(),
-                StepRow(),
-                UploadButton(),
-              ],
+        child: SizedBox(
+          width: 100.w,
+          height: 100.h,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            //clipBehavior: Clip.hardEdge,
+            child: Container(
+              //margin: EdgeInsets.only(left: (100.w - 1440.px) / 2),
+              width: 1440,
+              height: 1024,
+              child: Stack(
+                children: const [
+                  BackgroundWidget(),
+                  GreetingWidget(),
+                  StepRow(),
+                  UploadButton(),
+                ],
+              ),
             ),
           ),
         ),
