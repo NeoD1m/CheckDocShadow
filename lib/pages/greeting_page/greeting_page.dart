77de5e18443652e3bec90ba7464a55ac146bdf64
admin <roman.wowsq@gmail.com>
2022-10-14 22:10:35 +0300

    Скейлер

--- lib/pages/greeting_page/greeting_page.dart
+++ lib/pages/greeting_page/greeting_page.dart
@@ -3,37 +3,22 @@ import 'package:checkdoc/pages/greeting_page/greeting_widget.dart';
 import 'package:checkdoc/pages/greeting_page/step_row.dart';
 import 'package:checkdoc/pages/greeting_page/upload_button.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../../widgets/checkdoc_scaler.dart';
 
 class GreetingPage extends StatelessWidget {
   const GreetingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: 100.w,
-          height: 100.h,
-          child: FittedBox(
-            fit: BoxFit.contain,
-            //clipBehavior: Clip.hardEdge,
-            child: Container(
-              //margin: EdgeInsets.only(left: (100.w - 1440.px) / 2),
-              width: 1440,
-              height: 1024,
-              child: Stack(
-                children: const [
-                  BackgroundWidget(),
-                  GreetingWidget(),
-                  StepRow(),
-                  UploadButton(),
-                ],
-              ),
-            ),
-          ),
-        ),
+    return const Scaffold(
+      body: CheckDocScaler(
+        children: [
+          BackgroundWidget(),
+          GreetingWidget(),
+          StepRow(),
+          UploadButton(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/checkdoc_scaler.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class CheckDocScaler extends StatelessWidget {
+  const CheckDocScaler({super.key, required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 100.w,
+        height: 100.h,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: SizedBox(
+            width: 1440,
+            height: 1024,
+            child: Stack(
+              children: children,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
