52b204757e65bae9a814cb4d5f78fa7d7ce05383
NeoD1m <duffman7915492@gmail.com>
2022-10-17 01:58:07 +0300

    обновление todo

--- lib/widgets/check_doc_appbar.dart
+++ lib/widgets/check_doc_appbar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// TODO как хочешь делай аппбар, через встроенный appBar или через stack
+/// TODO поменять png на svg
 class CheckDocAppBar extends StatelessWidget {
   const CheckDocAppBar({super.key});
 
@@ -9,11 +9,11 @@ class CheckDocAppBar extends StatelessWidget {
     return Stack(
       children: [
         Container(
-          margin: EdgeInsets.only(left: 24, top: 16),
+          margin: const EdgeInsets.only(left: 24, top: 16),
           child: Image.asset("assets/images/leading.png", width: 48, height: 36, fit: BoxFit.contain),
         ),
         Container(
-          margin: EdgeInsets.only(left: 88, top: 26),
+          margin: const EdgeInsets.only(left: 88, top: 26),
           child: Image.asset("assets/images/checkDOC.png", width: 101.78, height: 20),
         ),
       ],
